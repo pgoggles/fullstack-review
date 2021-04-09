ab2ca5987a057857856117203879eebff70fc4af
pgoggles <praneeth.gogineni@gmail.com>
2021-04-09 14:16:30 -0700

    Add schema design

--- database/index.js
+++ database/index.js
@@ -3,6 +3,26 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  name: String,
+  id: Number,
+  url: String,
+  forks: {
+    forkCount: Number,
+    forkUrl: String
+  },
+  watchers: Number,
+  openIssues: {
+    issueCount: Number,
+    issueUrl: String,
+  },
+  download: {
+    hasDownloads: Boolean,
+    downloadUrl: String,
+  },
+  owner: {
+    url: String,
+    profilePicture: String,
+  }
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
@@ -11,6 +31,6 @@ let save = (/* TODO */) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-}
+};
 
 module.exports.save = save;

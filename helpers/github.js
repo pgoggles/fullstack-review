fd7971286fdfd545f16c784805f8617d93dff4b4
pgoggles <praneeth.gogineni@gmail.com>
2021-04-09 14:15:47 -0700

    Add properties to make a get request to GitHub API

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,22 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username, callback = (data) => console.log(data.data)) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos?type=owner`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
-    }
+    },
+    method: 'GET'
   };
-
-}
+  axios(options)
+    .then(callback);
+};
 
 module.exports.getReposByUsername = getReposByUsername;
